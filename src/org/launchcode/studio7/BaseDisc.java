52758304aaf71c79b6c16f65fd159e9f1f83cccd
lashundra <lashundrasdavis@att.net>
2023-04-24 17:45:09 -0500

    inal code for studio

---
+++ src/org/launchcode/studio7/BaseDisc.java
@@ -0,0 +1,51 @@
+package org.launchcode.studio7;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public abstract class BaseDisc {
+    private String name;
+    private int storageCapacity;
+    private int remainingStorageCapacity;
+    private int capacityUsed;
+    private String discType;
+    private ArrayList<String> contents;
+
+
+    public BaseDisc(String aName, int maxCapacity, String aType,
+                    int someUsedCapacity){
+        name = aName;
+        storageCapacity = maxCapacity;
+        discType = aType;
+        capacityUsed = checkCapacity(someUsedCapacity);
+        remainingStorageCapacity = spaceLeft();
+    }
+    private int checkCapacity(int dataWritten){
+        if (storageCapacity < dataWritten){
+            return storageCapacity;
+        }
+        return dataWritten;
+    }
+
+    private int spaceLeft(){
+        return storageCapacity - capacityUsed;
+    }
+
+    public String discInfo(){
+        String output = String.format("\nDisk name: %s\nMax capacity: %d" +
+                "\nSpace used: %d\n", name, storageCapacity,
+                capacityUsed, remainingStorageCapacity);
+        return output;
+    }
+
+    public String writeData(int dataSize){
+        if (dataSize > remainingStorageCapacity){
+            return "Not enough disc space!";
+        }
+        capacityUsed += dataSize;
+        remainingStorageCapacity -= dataSize;
+
+        return "Data written to disc. Remaining space = " + remainingStorageCapacity;
+
+    }
+}
